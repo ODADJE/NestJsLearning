2a99cc8009b18c51c7975f7b07423f2cc708ff2b
ODADJE <odadjerk@gmail.com>
2023-09-08 23:33:01 +0000

    Creation and work on todo module

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+
+@Controller('todo')
+export class TodoController {
+  @Get()
+  getTodos() {
+    console.log('Recuperer la liste des todos');
+    return 'Liste des TODOS';
+  }
+
+  @Post()
+  addTodos() {
+    console.log('Ajouter un todo la liste des todos');
+    return 'Add TODO';
+  }
+
+  @Delete()
+  deletteTodos() {
+    console.log('Supprimer un todo la liste des todos');
+    return 'Delete TODO';
+  }
+
+  @Put()
+  modifierTodos() {
+    console.log('Modifier un todo la liste des todos');
+    return 'Update TODO';
+  }
+}
